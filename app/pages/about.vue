02f8781047417cd264bb651c7f6566608317b050
WernervanderMerwe <wernervandermerwe@rocketmail.com>
2025-06-08 21:24:58 +0200

    finish Essentials Section

---
+++ app/pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>this page is about me</div>
+  <!-- <div>
+    <button @click="enableCustomLayout">Update Layout</button>
+  </div> -->
+</template>
+
+<script setup>
+// definePageMeta({
+//   layout: "another",
+// });
+// function enableCustomLayout () {
+//     setPageLayout('another');
+// }
+</script>
