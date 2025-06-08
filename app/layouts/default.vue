aaa8ac7de29a167014604d40a00ba65e7972b407
WernervanderMerwe <wernervandermerwe@rocketmail.com>
2025-06-08 23:49:11 +0200

    add tailwindcss and make a menu component

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,18 +1,17 @@
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <NuxtLink to="/">Main</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/about">About</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/blog">Blog</NuxtLink>
-        </li>
-      </ul>
-    </nav>
-    <slot />
+  <div class="container mx-auto 2xl">
+    <header class="flex justify-between items-center mt-5">
+      <div>
+        <NuxtLink
+          to="/"
+          class="text-xl font-semibold p-2 hover:bg-gray-200">
+          Werner van der Merwe
+        </NuxtLink>
+      </div>
+      <Menu />
+    </header>
+    <main class="p-2">
+      <slot />
+    </main>
   </div>
 </template>
